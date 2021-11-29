3e9657c204b8ddbc6c7d67349abc73236ebad123
PaulVili <77621022+PaulVili@users.noreply.github.com>
2021-11-29 12:50:13 +0300

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,14 @@
+const heading = document.querySelector('#exampleFormControlInput1')
+const textarea = document.querySelector('#exampleFormControlTextarea1')
+const check = document.querySelector('#flexCheckDefault')
+const select = document.querySelector('#inputState')
+const DateInput = document.querySelector('#DateInput')
+const AddMessageBtn = document.querySelector('.AddMessageBtn')
+const addCategoryInput = document.querySelector('.addCategoryInput')
+const addCategoryBtn = document.querySelector('.addCategoryBtn')
+
+
+
+AddMessageBtn.addEventListener('click', function(){
+    
+})
